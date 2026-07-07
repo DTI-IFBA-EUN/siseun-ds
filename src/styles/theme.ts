b32dce18cc07ed7ef1dd613f3a19e7bb58507c18
Arthur V. M. Dantas <arthur.dantas@ifba.edu.br>
2026-07-07 17:14:10 -0300

    adição da fonte Raleway usando a biblioteca font source

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,7 @@ import { createTheme } from "@mantine/core";
 
 const theme = createTheme({
   /** Configuração do tema do Mantine */
+  fontFamily: "Raleway Variable, sans-serif"
 });
 
 export default theme;

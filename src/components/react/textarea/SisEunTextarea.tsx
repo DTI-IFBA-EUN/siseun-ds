48e0b319b0ebbeb11f9c9f27c4b223504e4feea1
Vanderson Nogueira <202513600033@ifba.edu.br>
2026-08-12 18:14:41 -0300

    Progredi dentre a implementação do parâmetro bottomSection.

--- src/components/react/textarea/SisEunTextarea.tsx
+++ src/components/react/textarea/SisEunTextarea.tsx
@@ -1,5 +1,7 @@
 import { Textarea } from '@mantine/core';
 import type { MantineSize } from "@mantine/core";
+import { boolean, maxLength, number } from 'astro:schema';
+import { useState } from 'react';
 
 interface SisEunTextareaProps {
   size?: MantineSize;
@@ -7,19 +9,29 @@ interface SisEunTextareaProps {
   label?: React.ReactNode;
   placeholder?: string;
   hasAuxiliarText?: boolean;
-  maxLenght?: number;
+  maxLength?: number;
+  value?: string;
 }
 
 export function SisEunTextarea(props: SisEunTextareaProps) {
     const size = props.size ?? "md";
     const Resize = props.isResizeble ? "both" : "none";
 
-
-
-
+    const [value, setValue] = useState(props.value ?? '');
+    const maxLength = props.maxLength ?? Number.POSITIVE_INFINITY;
+    
+    console.log(value.length)
     return(
-        <Textarea size={size} label={props.label} placeholder={props.placeholder} resize={Resize}>
-            
+        <Textarea 
+            size={size}
+            label={props.label}
+            placeholder={props.placeholder}
+            resize={Resize}
+            value={value}
+            maxLength={props.maxLength}
+            onChange={(event) => setValue(event.currentTarget.value.slice(0, maxLength))}
+            bottomSection={`Restam ${maxLength - value.length}`}
+            >
         </Textarea>
     )
 }
